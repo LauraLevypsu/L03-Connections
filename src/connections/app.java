e4a97f9371e2a850926150cea1d37855e0606906
lauralevypsu <llevy510@gmail.com>
2018-05-28 14:18:54 -0400

    repo created, main class has an example from canvas to work off of

---
+++ src/connections/app.java
@@ -0,0 +1,24 @@
+package connections;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class app {
+
+
+    public static void main(String args[])
+    {
+        try
+        {
+            InetAddress addr = InetAddress.getByName("www.psu.edu");
+            System.out.println(addr.getCanonicalHostName());
+            System.out.println(addr.getHostAddress());
+            System.out.println(addr.isReachable(5000));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
