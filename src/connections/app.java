676d817519a3379fb3c42175354cab62e736a012
Adriannosaurus <Adriannosaurus@adrianncomputer.fios-router.home>
2018-06-02 13:32:32 -0400

    Background info commented in

--- src/connections/app.java
+++ src/connections/app.java
@@ -2,6 +2,7 @@ package connections;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URL;
 
 public class app
 {
@@ -9,10 +10,14 @@ public class app
     {
         try
         {
-            InetAddress addr = InetAddress.getByName("www.psu.edu");
-            System.out.println(addr.getCanonicalHostName());
-            System.out.println(addr.getHostAddress());
-            System.out.println(addr.isReachable(5000));
+            InetAddress addr = InetAddress.getByName("www.ncdc.noaa.gov/cdo-web/api/v2/datasets");
+            //NOAA Token if someone figures that out: wyYHemlmRuFKCNLHqauEjyhsqDwLDfMR
+            System.out.println("Canon Host Name: " + addr.getCanonicalHostName());
+            System.out.println("Host Name: " + addr.getHostName());
+            System.out.println("Host Address: " + addr.getHostAddress());
+            System.out.println("Reachable: " + addr.isReachable(5000));
+            
+            
         }
         catch(IOException e)
         {
@@ -22,4 +27,16 @@ public class app
     }
 }
 
-// Test commit from @Adriannosaurus
+/*
+
+Twitter info: https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets.html
+
+Twitter API get request
+$ curl --request GET 
+ --url 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular' 
+ --header 'authorization: OAuth oauth_consumer_key="consumer-key-for-app", 
+ oauth_nonce="generated-nonce", oauth_signature="generated-signature", 
+ oauth_signature_method="HMAC-SHA1", oauth_timestamp="generated-timestamp", 
+ oauth_token="access-token-for-authed-user", oauth_version="1.0"'
+
+*/
